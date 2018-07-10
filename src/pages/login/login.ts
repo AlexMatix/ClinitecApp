dad48eff6e96f7e24955a5f99321bfba6b35ad6b
Sebastian Vargas Barrera <sebastian.vargas@interglobalcs.com>
2018-07-10 17:57:58 -0500

    Specialities

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { SpecialtiesPage } from '../specialties/specialties';
 import { RegisterPage } from '../register/register';
 import { ForgetPage } from '../forget/forget';
 
-import { Observable } from 'rxjs/Observable';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { SERVER_URL, ACCESS_TOKEN } from '../../providers/constants/constants';
+import { SERVER_URL, ACCESS_TOKEN, SECRET } from '../../providers/constants/constants';
 
 @IonicPage()
 @Component({
@@ -19,13 +19,14 @@ export class LoginPage {
   pass: string = "";
   url: string = "";
   token: string = "";
+  secret: string = "";
   headers = new HttpHeaders();
   login: Observable<any>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
     this.url = SERVER_URL;
     this.token = ACCESS_TOKEN;
-
+    this.secret = SECRET;
   }
 
   ionViewDidLoad() {
@@ -38,13 +39,11 @@ export class LoginPage {
     let userInfo = {
       grant_type:"password",
       client_id: "1",
-      client_secret:"5prs81bNv9aEugCEJ8SdbQIjKbg2geRBP3Dd4dHQ",
+      client_secret: this.secret,
       username: this.username,
       password: this.pass,
       Destino: "ClientMovil"
     }
-
-
 
     this.login = this.http.post(`${this.url}/oauth/token`, userInfo, {
       headers: {
@@ -52,11 +51,13 @@ export class LoginPage {
         "Authorization": `Bearer ${this.token}`
       }
     });
+
     this.login
     .subscribe(data => {
       console.log(data);
-      localStorage.setItem('access_token', data.access_token)
-      localStorage.setItem('refresh_token', data.refresh_token)
+      localStorage.setItem('access_token', data.access_token);
+      localStorage.setItem('refresh_token', data.refresh_token);
+      localStorage.setItem('email', this.username);
       this.navCtrl.setRoot(SpecialtiesPage);
     })
 
--- src/pages/specialties/specialties.ts
+++ src/pages/specialties/specialties.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 import { DoctorsPage } from '../doctors/doctors';
 import { DatingPage } from '../dating/dating';
@@ -7,15 +9,50 @@ import { PatientProfilePage } from '../patient-profile/patient-profile';
 import { DoctorProfilePage } from '../doctor-profile/doctor-profile';
 import { AllPatientsPage } from '../all-patients/all-patients';
 
+import { SERVER_URL } from '../../providers/constants/constants';
+
 @IonicPage()
 @Component({
   selector: 'page-specialties',
   templateUrl: 'specialties.html',
 })
 export class SpecialtiesPage {
+  url: string = "";
+  email: string = "";
+  mcenter_id: string = "";
+  headers = new HttpHeaders();
+  userinfo: Observable<any>;
+  specialties: Observable<any>;
 
-  constructor(public menu: MenuController, public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public menu: MenuController, public navCtrl: NavController, public navParams: NavParams,
+              public http: HttpClient) {
+
+    this.email = localStorage.getItem('email');
     this.menu.enable(true);
+    this.url = SERVER_URL;
+
+
+    this.userinfo = this.http.get(`${this.url}/user-information/${this.email}`, {
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem('access_token')}`
+      }
+    });
+    this.userinfo
+    .subscribe(data => {
+      console.log(data);
+      this.mcenter_id = data.User.idCentro_medico;
+    });
+
+    this.specialties = this.http.get(`${this.url}/especialidades/${this.mcenter_id}`, {
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem('access_token')}`
+      }
+    });
+    this.specialties
+    .subscribe(data => {
+      console.log(data);
+    });
+
   }
 
   ionViewDidLoad() {
