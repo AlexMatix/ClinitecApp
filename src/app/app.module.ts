9292a46e5980931bba64aa99eb1bbfa829879f4e
Luis Angel Osorio <luis.angel.osorio@outlook.com>
2019-05-29 14:14:48 -0500

    Corrección android 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-import { LocalNotifications } from '@ionic-native/local-notifications';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {LocalNotifications} from '@ionic-native/local-notifications';
 
-import { MyApp } from './app.component';
-import { LoginPage } from '../pages/login/login';
-import { RegisterPage } from '../pages/register/register';
-import { SpecialtiesPage } from '../pages/specialties/specialties';
-import { ForgetPage } from '../pages/forget/forget';
-import { DoctorsPage } from '../pages/doctors/doctors';
-import { DprofilePage } from '../pages/dprofile/dprofile';
-import { DatingPage } from '../pages/dating/dating';
-import { PatientProfilePage } from '../pages/patient-profile/patient-profile';
-import { DoctorProfilePage } from '../pages/doctor-profile/doctor-profile';
-import { AllPatientsPage } from '../pages/all-patients/all-patients';
-import { RecipesPage } from '../pages/recipes/recipes';
+import {MyApp} from './app.component';
+import {LoginPage} from '../pages/login/login';
+import {RegisterPage} from '../pages/register/register';
+import {SpecialtiesPage} from '../pages/specialties/specialties';
+import {ForgetPage} from '../pages/forget/forget';
+import {DoctorsPage} from '../pages/doctors/doctors';
+import {DprofilePage} from '../pages/dprofile/dprofile';
+import {DatingPage} from '../pages/dating/dating';
+import {PatientProfilePage} from '../pages/patient-profile/patient-profile';
+import {DoctorProfilePage} from '../pages/doctor-profile/doctor-profile';
+import {AllPatientsPage} from '../pages/all-patients/all-patients';
+import {RecipesPage} from '../pages/recipes/recipes';
 
-import { HttpClientModule } from '@angular/common/http';
-
-import { CapitalizePipe } from '../pipes/capitalize/capitalize';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    MyApp
+    MyApp,
+    LoginPage,
+    AllPatientsPage,
+    DatingPage,
+    DoctorProfilePage,
+    DoctorsPage,
+    ForgetPage,
+    LoginPage,
+    PatientProfilePage,
+    RecipesPage,
+    RegisterPage,
+    SpecialtiesPage,
+    DprofilePage
   ],
   imports: [
     BrowserModule,
